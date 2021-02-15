6c4afddd107b45d213b8c14e7c28dd9d3594b3dd
JacobMason83 <jmason062083@gmail.com>
2021-02-15 14:49:44 -0500

    added add to cart and remove funcitonality, and restyled the cards

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,41 @@
-let AddToCart = document.getElementsByClassName('cart-btn')
 
-for(let i = 0; i < AddToCart.length; i++){
-    let  button = AddToCart[i]
-    button.addEventListener('click',  function() {
-        console.log('you clicked me')
-    })
-}
+const addNumberToCart = () => {
+    const cartAdded = document.getElementsByClassName('card-btn')
+    for(var i = 0; i < cartAdded.length; i++){
+      const button = cartAdded[i]
+      let count = document.getElementById('count').value || 0
+      button.addEventListener('click', () => {
+        const addtoDiv = document.getElementById('count')
+        if(typeof addtoDiv.value === Number ){
+          count = parseInt(count )
+          addtoDiv.value = count + 1
+        } else {
+            count  = parseInt(count)
+            addtoDiv.value = count + 1
+        }
+      }) 
+  }
+  }
+const removeItemfromCart = () => {
+     const removeButtons = document.getElementsByClassName('remove-btn')  
+     for(var i = 0; i < removeButtons.length; i++ ){
+         const button = removeButtons[i]
+         let count = document.getElementById('count').value 
+         button.addEventListener('click', () => {
+             const removeFromDiv = document.getElementById('count')
+             if( typeof removeFromDiv.value === Number) {
+                  decrementCount = parseInt(count)
+                   removeFromDiv.value = decrementCount - 1
+                } else {
+                    decrementCount = parseInt(count)
+                    removeFromDiv.value = decrementCount - 1
+                }
+         })
+     }
+  }
+  const popup = () => {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -17,17 +17,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-const cartAdded = document.getElementsByClassName('card-btn')
-for(var i = 0; i < cartAdded.length; i++){
-  const button = cartAdded[i]
-  let count = 0
-  button.addEventListener('click', () => {
-    const addtoDiv = document.getElementsByClassName('.count-custom')
-    if(addtoDiv.innerHTML === null ){
-      addtoDiv.innerHTML = count + 1
-    } else {
-      addtoDiv.innerHTML = count + 1
-    }
-  })
-}
